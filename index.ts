086f7828570f40f3120fdd43efaf21d1bd8204e7
kenedy <kendowcent@gmail.com>
2024-06-13 08:50:02 -0400

    integracion de los audios

--- index.ts
+++ index.ts
@@ -1,89 +1,3 @@
-// import { createServer, Server as HTTPServer } from "http";
-// import { Server as SocketIOServer, Socket } from "socket.io";
-// import cors from 'cors';
-
-// const httpServer: HTTPServer = createServer();
-// const io: SocketIOServer = new SocketIOServer(httpServer, {
-//   // options
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"]
-//   }
-// });
-
-// // Escuchamos el evento "connection" para manejar las conexiones de los clientes
-// io.on("connection", (socket: Socket) => {
-//   // Enviamos un mensaje de bienvenida al cliente que se ha conectado
-//   socket.emit("message", "¡Bienvenido al servidor de Socket.IO!");
-
-//   // Mostramos un mensaje en la consola del servidor cuando un cliente se conecta
-//   console.log("Cliente conectado");
-
-//   // Escuchamos el evento "message" desde el cliente y mostramos el mensaje recibido en la consola del servidor
-//   socket.on("message", (data: string) => {
-//     console.log("Mensaje recibido del cliente:", data);
-//   });
-// });
-
-// // Escuchamos el evento "listening" del servidor HTTP para obtener el puerto en el que está escuchando
-// httpServer.on("listening", () => {
-//   const addr = httpServer.address();
-//   const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr?.port;
-//   console.log("Servidor HTTP escuchando en " + bind);
-// });
-
-// // Ponemos a escuchar el servidor HTTP en el puerto 4000
-// httpServer.listen(4000);
-
-
-// import { createServer, Server as HTTPServer } from "http";
-// import { Server as SocketIOServer, Socket } from "socket.io";
-// import cors from 'cors';
-
-// const httpServer: HTTPServer = createServer();
-// const io: SocketIOServer = new SocketIOServer(httpServer, {
-//   // options
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"]
-//   }
-// });
-
-// // Escuchamos el evento "connection" para manejar las conexiones de los clientes
-// io.on("connection", (socket: Socket) => {
-//   // Enviamos un mensaje de bienvenida al cliente que se ha conectado
-//   socket.emit("message", "¡Bienvenido al servidor de Socket.IO!");
-
-//   // Mostramos un mensaje en la consola del servidor cuando un cliente se conecta
-//   console.log("Cliente conectado");
-
-//   // Escuchamos el evento "message" desde el cliente y mostramos el mensaje recibido en la consola del servidor
-//   socket.on("message", (data: string) => {
-//     console.log("Mensaje recibido del cliente:", data);
-//     // Emitimos el mensaje recibido a todos los clientes conectados, incluyendo al cliente que lo envió
-//     io.emit("message", data);
-//   });
-// });
-
-// // Escuchamos el evento "listening" del servidor HTTP para obtener el puerto en el que está escuchando
-// httpServer.on("listening", () => {
-//   const addr = httpServer.address();
-//   const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr?.port;
-//   console.log("Servidor HTTP escuchando en " + bind);
-// });
-
-// // Ponemos a escuchar el servidor HTTP en el puerto 4000
-// httpServer.listen(4000);
-
-
-
-// server.ts
-
-// server.ts
-
-// server.ts
-
-// server.ts
 
 
 import express from 'express';
@@ -91,9 +5,24 @@ import http from 'http';
 import { Server as SocketIOServer, Socket } from 'socket.io';
 import mongoose, { Document, Schema, Model } from 'mongoose';
 import cors from 'cors';
+import multer from 'multer';
+import path from 'path';
+
+// Configuración de multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
 
 interface IMessage extends Document {
-  text: string;
+  text?: string;
+  audioUrl?: string;
   ip: string;
   createdAt: Date;
 }
@@ -103,7 +32,7 @@ const server = http.createServer(app);
 const io = new SocketIOServer(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"]
+    methods: ["GET", "POST"]
   }
 });
 
@@ -116,10 +45,11 @@ mongoose.connect("mongodb+srv://monk:juventus@cluster0.rocdkbv.mongodb.net/?retr
   });
 
 app.use(cors());
-app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 
 const messageSchema = new Schema({
   text: String,
+  audioUrl: String,
   ip: String,
   createdAt: { type: Date, default: Date.now }
 });
@@ -130,57 +60,39 @@ app.get('/', (req, res) => {
   res.send('<h1>Bienvenido al servidor yeah!</h1>');
 });
 
-app.get('/api/messages', async (req, res) => {
-  try {
-    const messages = await MessageModel.find().sort({ createdAt: -1 });
-    res.json(messages);
-  } catch (error) {
-    res.status(500).send('Error al obtener mensajes');
-  }
-});
-
-app.post('/api/messages', async (req, res) => {
-  try {
-    const messageData = req.body;
-    const message = new MessageModel(messageData);
-    await message.save();
-    res.status(201).json(message);
-  } catch (error) {
-    res.status(500).send('Error al crear mensaje');
-  }
-});
-
-app.put('/api/messages/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updatedData = req.body;
-    const message = await MessageModel.findByIdAndUpdate(id, updatedData, { new: true });
-    res.json(message);
-  } catch (error) {
-    res.status(500).send('Error al actualizar mensaje');
-  }
-});
-
-app.delete('/api/messages/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    await MessageModel.findByIdAndDelete(id);
-    res.status(204).send();
-  } catch (error) {
-    res.status(500).send('Error al eliminar mensaje');
-  }
-});
-
-io.on('connection', (socket: Socket) => {
+io.on('connection', async (socket: Socket) => {
   console.log(`Usuario conectado desde la dirección IP: ${socket.handshake.address}`);
 
-  socket.on('signal', (data) => {
-    socket.to(data.to).emit('signal', { from: socket.id, signal: data.signal });
+  try {
+    const messages = await MessageModel.find().limit(15).sort({ createdAt: -1 });
+    socket.emit('messages', messages.reverse());
+  } catch (error) {
+    console.error('Error al buscar mensajes:', error);
+  }
+
+  socket.on('message', async (data: Partial<IMessage>) => {
+    const ipAddress = socket.handshake.address;
+    const messageData = { ...data, ip: ipAddress };
+    const message = new MessageModel(messageData);
+    try {
+      await message.save();
+      io.emit('message', message);
+    } catch (error) {
+      console.error('Error al guardar mensaje:', error);
+    }
   });
 
   socket.on('disconnect', () => {
     console.log('Usuario desconectado');
   });
+});
+
+app.post('/upload', upload.single('audio'), (req, res) => {
+  const file = req.file;
+  if (!file) {
+    return res.status(400).send('No se subió ningún archivo.');
+  }
+  res.status(200).json({ audioUrl: `/uploads/${file.filename}` });
 });
 
 server.listen(4000, () => {
